d9601c4a44594a743bb65ba32187e217c5e58db3
James Cockbain <james.cockbain@ibm.com>
2019-11-22 20:55:24 +0000

    add id tests
    Signed-off-by: James Cockbain <james.cockbain@ibm.com>

---
+++ test/setup/chai.setup.js
@@ -0,0 +1,6 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+
+chai.use(chaiHttp);
+
+module.exports = chai;
---
+++ test/setup/server.setup.js
@@ -0,0 +1,10 @@
+const app = require("../../src/app");
+const chai = require("./chai.setup");
+
+module.exports = () => chai.request(app);
+
+/*
+To make request:
+const server = require('../setup/server.setup');
+const res = await app.get('/);
+*/
